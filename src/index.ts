a5bd675e6008e1b198513ba2fa66a8787b1a0399
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-12-17 10:17:31 -0500

    PIXI implementation
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import * as _ from 'lodash';
-import {testFunc} from './testFunc'; 
+import * as PIXI from 'pixi.js'; 
 
-function component() {
+let app = new PIXI.Application({ width: 640, height: 360 });
+document.body.appendChild(app.view);
+let sprite = PIXI.Sprite.from('../assets/img/cat.png');
+app.stage.addChild(sprite);
 
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Nuevo', 'webpack'], ' ');
-
-  return element;
-}
-
-console.log(testFunc(4));
-
-document.body.appendChild(component());
+ // Add a variable to count up the seconds our demo has been running
+ let elapsed = 0.0;
+ // Tell our application's ticker to run a new callback every frame, passing
+ // in the amount of time that has passed since the last tick
+ app.ticker.add((delta) => {
+   // Add the time to our total elapsed time
+   elapsed += delta;
+   // Update the sprite's X position based on the cosine of our elapsed time.  We divide
+   // by 50 to slow the animation down a bit...
+   sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+ });
